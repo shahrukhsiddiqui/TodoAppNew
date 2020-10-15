4a043960c6eae7fef2a4183f06d647f3dd8c2916
shahrukhsiddiqui <shahrukhsiddiqui45@gmail.com>
2020-10-15 13:07:11 +0530

    todo app changes

--- src/App.js
+++ src/App.js
@@ -1,92 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoForm from './TodoForm/TodoForm'
-import Todo from './TodoForm/Todo'
 
 class App extends Component
 {
-  state ={
-    todos:[],
-    todosToShow:"all"
-  };
-
-addTodo = todo  =>{
-this.setState({
-  todos:[todo,...this.state.todos]
-});
-}
-
-toggleComplete = (id) =>{
-  this.setState({
-    todos:this.state.todos.map(todo =>{
-      if(todo.id === id){
-        return {
-         ...todo,
-          complete:!todo.complete
-        }
-      }
-        else {
-          return todo;
-        }
-      
-    })
-  })
-}
-
-updateTodosToShow = (s) =>{
-  this.setState({
-    todosToShow:s
-  })
-}
-
-handleDelete = id =>{
-  this.setState({
-    todos:this.state.todos.filter(todo => todo.id !== id)
-  });
-};
-
-removeAllCompletedTodos = () =>{
-  this.setState({
-    todos:this.state.todos.filter(todo => !todo.complete)
-  });
-};
+  
 
   render(){
-    let todos = [];
+   
 
-    if(this.state.todosToShow === "all"){
-       todos = this.state.todos;
-    }
-    else if (this.state.todosToShow === "Active"){
-      todos = this.state.todos.filter(todo => !todo.complete);
-      
-    }
-    else if (this.state.todosToShow === "Completed"){
-      todos = this.state.todos.filter(todo => todo.complete);
-    }
+    
 
     return(
       <div>
-      <TodoForm onSubmit = {this.addTodo}></TodoForm>
-      {todos.map(todo =>(
-        <Todo key = {todo.id} 
-        toggleComplete = {() => this.toggleComplete(todo.id)}
-        onDelete={()=>this.handleDelete(todo.id)}
-        todo = {todo}></Todo>
-      ))}
-      <div>
-        todos left:{this.state.todos.filter(todo => !todo.complete).length}
-      </div>
-      <button onClick = {() =>this.updateTodosToShow("all")}>All</button>
-      <button onClick = {() =>this.updateTodosToShow("Active")}>Active</button>
-      <button onClick = {() =>this.updateTodosToShow("Completed")}>Completed</button>
-      {this.state.todos.filter(todo => todo.complete).length?
-      <button onClick = {() =>this.removeAllCompletedTodos()}>Remove Completed</button>
-      :null}
+      <h1>ghh</h1>
       
       </div>
     )
-  }
+    }
 }
+
 
 export default App;
